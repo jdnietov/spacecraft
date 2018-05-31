21bee750e62fa202c2d4303c40c4b7bf4b0f0317
José Nieto <jdnv.123@gmail.com>
2018-05-31 02:42:58 -0500

    Declare basic GUI components

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
+import kivy
+kivy.require('1.9.1')
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.filechooser import FileChooserListView
+from kivy.lang import Builder
+
 import os
 
 from dirstrs import isnpm
-from gui import Spacecraft
+from templateWidget import TemplateWidget
 from blazer import ProjectFile, BlazeFunction, createmodule
+
+Builder.load_file('customfilechooserview.kv')
+Builder.load_file('scgrid.kv')
+Builder.load_file('templatewidget.kv')
 
 PROJECT_PATH="/home/jdnietov/Development/meteorapp"
 PROJECT_FILES=[]
+
+class CustomFileChooserView(FileChooserListView):
+    pass
+
+class TemplateWidget(Widget):
+    pass
+
+class ScGrid(GridLayout):
+    pass
+
+class SpacecraftApp(App):        
+    def build(self):
+        self.title = 'Spacecraft! - software visualizer for Meteor.js projects'
+        return ScGrid()
 
 def main():
     # PROJECT_PATH=raw_input()
@@ -16,7 +43,7 @@ def main():
             PROJECT_FILES.append(f)            
             break
     
-    Spacecraft().run()
-    
+    spacecraft = SpacecraftApp()
+    spacecraft.run()    
 
 main()
---
+++ templateWidget.py
@@ -0,0 +1,7 @@
+import kivy
+kivy.require('1.9.1')
+
+from kivy.uix.widget import Widget
+
+class TemplateWidget(Widget):
+    pass
