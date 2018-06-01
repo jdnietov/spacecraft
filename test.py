a870848ea51e9b30b290e055f45bcc6fa4744cfc
José Nieto <jdnv.123@gmail.com>
2018-06-01 03:08:50 -0500

    Delete test

--- test.py
+++
@@ -1,7 +0,0 @@
-import esprima
-
-PATH="/home/jdnietov/Development/meetin/imports/ui/pages/activityPurchase.js"
-code = open(PATH, "r")
-tree = esprima.parseModule(code.read())
-code.close()
-print(tree)
